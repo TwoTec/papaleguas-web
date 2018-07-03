fcedb6ddbc814d0a54772969eb4e053de8f1263d
gilTads <gil.tads@gmail.com>
2018-07-03 17:56:41 -0400

    criando esboço Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,56 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
     <v-navigation-drawer
-    fixed app>
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="0">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="0">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-title>Papaléguas</v-toolbar-title>
+    <v-toolbar color="cyan darken-1" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
     </v-toolbar>
     <v-content>
-      <router-view></router-view>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+        </v-layout>
+      </v-container>
     </v-content>
-    <v-footer app ></v-footer>
+    <v-footer color="cyan darken-1" app>
+      <span class="white--text">&copy; 2018</span>
+    </v-footer>
   </v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      title: 'Papléguas'
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import Login from './components/Login'
-
 import Cadastro  from './components/cliente/Cadastro'
+import Home from './components/Home'
 
 export const routes = [
   {
@@ -10,6 +10,9 @@ export const routes = [
   {
     path: '/cliente/cadastro',
     component: Cadastro
-
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
