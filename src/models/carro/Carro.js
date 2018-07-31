7557b22793bfc2d7dccf9fc121493c9ba431ad36
Ederson Souza <ederson.souza.mg@gmail.com>
2018-07-30 20:40:43 -0400

    alterando carro

--- src/models/carro/Carro.js
+++ src/models/carro/Carro.js
@@ -1,11 +1,12 @@
 export default class Carro{
-    constructor(modelo, ano, cor, placa, cidade, estado){
+    constructor(marca,modelo, ano, cor, placa, cidade, UF){
+        this.marca=marca;
         this.modelo=modelo;
         this.ano=ano;
         this.cor=cor;
         this.placa=placa;
         this.cidade=cidade;
-        this.estado=estado;
+        this.UF=UF;
     }
 
 }
